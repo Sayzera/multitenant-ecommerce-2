b637a267e8840f0e672b42a97d9c8eed4be81624
Sayzera <white.code.text@gmail.com>
2025-09-05 08:53:08 +0300

    Implement breadcrumb navigation and dynamic category background; refactor category handling in search filters.

--- src/app/(home)/search-filters/index.tsx
+++ src/app/(home)/search-filters/index.tsx
@@ -3,20 +3,47 @@ import { SearchInput } from "./search-input";
 import { Categories } from "./categories";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { DEFAULT_BG_COLOR } from "@/modules/home/constants";
+import {BreadcrumbNavigation} from "./breadcrumb-navigation";
 
 interface SearchFiltersProps {}
 export const SearchFilters = ({}: SearchFiltersProps) => {
+  const params = useParams();
+  const categoryParam = params.category as string ||undefined;
+  const activeCategory = categoryParam || 'all';
+
+
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+ 
+ 
+  const activeCategoryData = data.find((category) => category.slug === activeCategory);
+
+  const activeCategoryColor = activeCategoryData?.color as string || DEFAULT_BG_COLOR;
+  const activeCategoryName = activeCategoryData?.name || null;
+
+  const activeSubcategory = params.subcategory as string | undefined;
+  const activeSubcategoryName = activeCategoryData?.subcategories?.find(
+    (subcategory) => subcategory.slug === activeSubcategory
+  )?.name || null;
+
+
 
   return (
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full" style={{
-      backgroundColor: '#F5F5F5'
+      backgroundColor: activeCategoryColor,
     }}>
       <SearchInput disabled={false} data={data} />
       <div className="hidden lg:block">
         <Categories data={data} />
       </div>
+
+      <BreadcrumbNavigation
+        activeCategory={activeCategory}
+        activeCategoryName={activeCategoryName}
+        activeSubcategoryName={activeSubcategoryName} 
+      />
     </div>
   );
 };
